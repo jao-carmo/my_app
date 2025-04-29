b8df84d7369a1cdb69fc1f64fa60c5e4d0341ae8
jao-carmo <joaoepdocarmo@gmail.com>
2025-04-29 16:14:46 -0300

    four

---
+++ src/firebase/connection.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyBgqx7KLY5Q21S_EUMDDAVsi_K_evDdWDc",
+  authDomain: "hamburgueriafol.firebaseapp.com",
+  projectId: "hamburgueriafol",
+  storageBucket: "hamburgueriafol.firebasestorage.app",
+  messagingSenderId: "1045282744430",
+  appId: "1:1045282744430:web:5aac05c3c70300f75f9e7f",
+  measurementId: "G-REMXVFZFM6"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,41 +1,91 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import styles from '../styles/Register.module.css';
- 
+import { useAuthentication } from '../hooks/useAuthentication';
+
 const Register = () => {
+    const [displayName, setDisplayName] = useState('');
+    const [displayEmail, setEmail] = useState('');
+    const [displayPassword, setPassword] = useState('');
+    const [displayConfirm, setConfirm] = useState('');
+    const [error, setError] = useState('');
+    const {createUser, error: authError, loading} = useAuthentication();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+
+        if (displayPassword !== displayConfirm) {
+            setError("As senhas precisam ser iguais!");
+            return;
+        }
+        const res = await createUser(user)
+
+        const user = {
+            displayName,
+            displayEmail,
+            displayPassword
+        };
+
+        console.log(user);
+    };
+
     return (
-        <div>
+        <div className={styles.div_register}>
             <h2>Cadastre-se para ter Acesso ao Site</h2>
-            <form>
-                <label>
-                    <span>
-                        Nome:
-                    </span>
-                    <input type="text" name="displayName" required placeholder="Nome do Usuário" />
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <label className={styles.label}>
+                    <span>Nome:</span>
+                    <input
+                        type="text"
+                        name="displayName"
+                        required
+                        placeholder="Nome do Usuário"
+                        value={displayName}
+                        onChange={(e) => setDisplayName(e.target.value)}
+                        className={styles.input}
+                    />
                 </label>
-                <label>
-                    <span>
-                        E-mail:
-                    </span>
-                    <input type="email" name="displayName" required placeholder="E-mail" />
+                <label className={styles.label}>
+                    <span>E-mail:</span>
+                    <input
+                        type="email"
+                        name="displayEmail"
+                        required
+                        placeholder="E-mail"
+                        value={displayEmail}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className={styles.input}
+                    />
                 </label>
-                <label>
-                    <span>
-                        Senha:
-                    </span>
-                    <input type="password" name="displayName" required placeholder="Senha" />
+                <label className={styles.label}>
+                    <span>Senha:</span>
+                    <input
+                        type="password"
+                        name="displayPassword"
+                        required
+                        placeholder="Senha"
+                        value={displayPassword}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className={styles.input}
+                    />
                 </label>
-                <label>
-                    <span>
-                        Confirmar Senha:
-                    </span>
-                    <input type="password" name="displayName" required placeholder="Repetir Senha" />
+                <label className={styles.label}>
+                    <span>Confirmar Senha:</span>
+                    <input
+                        type="password"
+                        name="displayConfirm"
+                        required
+                        placeholder="Repetir Senha"
+                        value={displayConfirm}
+                        onChange={(e) => setConfirm(e.target.value)}
+                        className={styles.input}
+                    />
                 </label>
-                 <button className="btnCadastrar">
-                    Cadastrar
-                </button>
+                <button type="submit" className={styles.btnCadastrar}>Cadastrar</button>
+                {error && <p className={styles.error}>{error}</p>}
             </form>
         </div>
-    )
-}
- 
-export default Register
+    );
+};
+
+export default Register;
